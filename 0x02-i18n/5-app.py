b84298de29b696f596f1fd43567a2d1c082dbcb4
MahiET <meazalemma2020@gmail.com>
2022-07-19 13:29:40 +0000

    Task 5

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """ Route module for the API """
-from flask import Flask, render_template, request, g
+from flask import Flask, request, render_template, g
 from flask_babel import Babel
+from os import getenv
+from typing import Union
 
 
 users = {
@@ -24,35 +26,38 @@ class Config(object):
 
 
 # set the above class object as the configuration for the app
-app.config.from_object(Config)
+app.config.from_object('5-app.Config')
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index() -> str:
     """ GET /
     Return:
-      - 5-index.html
+      - 4-index.html
     """
     return render_template('5-index.html')
 
 
 @babel.localeselector
-def get_locale():
+def get_locale() -> str:
     ''' get locale from request '''
-    locale = request.args.get("locale")
-    if locale:
-        return locale
-    return request.accept_languages.best_match(Config.LANGUAGES)
+    if request.args.get('locale'):
+        locale = request.args.get('locale')
+        if locale in app.config['LANGUAGES']:
+            return locale
+        else:
+            return request.accept_languages.best_match(Config.LANGUAGES)
 
 
 def get_user() -> Union[dict, None]:
     """ Returns user dict if ID can be found """
-    login_as = request.args.get("login_as", False)
-    if login_as:
-        user = users.get(int(login_as), False)
-        if user:
-            return user
-    return None
+    if request.args.get('login_as'):
+        # have to type cast  the param to be able to search the user dict
+        user = int(request.args.get('login_as'))
+        if user in users:
+            return users.get(user)
+    else:
+        return None
 
 
 @app.before_request
@@ -62,4 +67,6 @@ def before_request():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+     host = getenv("API_HOST", "0.0.0.0")
+     port = getenv("API_PORT", "5000")
+     app.run(host=host, port=port)
